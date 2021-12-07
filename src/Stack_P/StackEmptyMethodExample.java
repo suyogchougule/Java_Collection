36f6ad2ab2205432136bb15dcf980c4f8f5f3da9
suyog <suyogchougule108@gmailcom>
2021-12-07 22:40:01 +0530

    new pakage stack

---
+++ src/Stack_P/StackEmptyMethodExample.java
@@ -0,0 +1,27 @@
+package Stack_P;
+import java.util.Stack;
+public class StackEmptyMethodExample 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//creating an instance of Stack class  
+		Stack<Integer> stk= new Stack<>();  
+		// checking stack is empty or not  
+		boolean result = stk.empty();  
+		System.out.println("Is the stack empty? " + result);  
+		// pushing elements into stack  
+		stk.push(78);  
+		stk.push(113);  
+		stk.push(90);  
+		stk.push(120);  
+		//prints elements of the stack  
+		System.out.println("Elements in Stack: " + stk);  
+		result = stk.empty();  
+		System.out.println("Is the stack empty? " + result);  
+	  
+
+	}
+
+}
